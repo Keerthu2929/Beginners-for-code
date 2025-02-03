7a022b8efc0740d6dc0d4efb1f57c05b18fd99fe
keerthu2929 <keerthanamuni29@gmail.com>
2025-02-03 20:09:46 +0530

    swap.py file added

---
+++ swap.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the value of x?"))
+y = int(input("Enter the value of y?"))
+x = x+y
+y = x-y
+x = x-y
+print("before swapping number X :",x)
+print("after swapping number Y :",y)
