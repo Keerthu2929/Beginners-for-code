40d7dd3e5f702446a27eba9d6e078abacf37060a
keerthu2929 <keerthanamuni29@gmail.com>
2025-02-03 17:00:11 +0530

    largest-of-Three.py file added

---
+++ Largest-of-Three.py
@@ -0,0 +1,9 @@
+Num1 = int(input("Enter your large num1:"))
+Num2 = int(input("Enter your large num2:"))
+Num3 = int(input("Enter your large num3:"))
+if Num2 <= Num1 >= Num3:
+  print("num1 is the largest number.")
+elif Num1 <= Num2 >= Num3:
+  print("num2 is the largest number.")
+else:
+  print("num3 is the largest number.")
