1fa2cc55aae27f31e8e6b2243e339c3908cde38a
keerthu2929 <keerthanamuni29@gmail.com>
2025-02-07 12:10:49 +0530

    error1.py file added

---
+++ error1.py
@@ -0,0 +1,5 @@
+# print("Number of letter in your name:" + len(input("Enter your name:"))) #error task line
+
+a = input("Enter your name:")
+length = len(a)
+print("Number of letter in your name:" + str(length) + ".")
