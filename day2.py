2c06c38f5404e803369e9398207b09de1c48b91e
keerthu2929 <keerthanamuni29@gmail.com>
2025-02-08 19:49:32 +0530

    day2.py file added

--- day2.py
+++ day2.py
@@ -17,3 +17,5 @@ bill_per_person = total_bill / people
 final_amount = round(bill_per_person,2)
 
 print(f"Each person should pay: ${final_amount}")
+
+print("Each person should pay: $"+" "+str(final_amount))
